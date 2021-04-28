122964cf23c7098b5ffc72b8aef470d12fd7e0e4
UlkraUach <a329842@uach.mx>
2021-04-27 19:57:01 -0500

    7º ejercicio

---
+++ ejercicio7.js
@@ -0,0 +1,23 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+var contador = 0;
+
+var recursiveAsyncReadLine = function () {
+    readline.question("Inserta una frase (/end para salir): "
+        , function (line) {
+            switch (line){
+                case "/end":
+                    console.log("Total: " + contador);
+                    return readline.close();
+                    break;
+                default:
+                    ++contador;
+            }
+    recursiveAsyncReadLine();
+    });
+};
+
+recursiveAsyncReadLine(); 
